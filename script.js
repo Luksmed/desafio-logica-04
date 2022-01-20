cf62d8b931b333680b6639efabe72ba37a3c9a07
Luksmed <lukksme@gmail.com>
2022-01-19 22:36:16 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,24 @@
+function msg(){
+    console.log("Hora do show")
+}
+msg()
+
+function meuNome(nome){
+    console.log(nome)
+}
+meuNome("Lucas")
+
+function perfil(nome, idade, estiloMusical){
+    console.log(`Me chamo ${nome}, tenho ${idade} anos, e meu estilo musical preferido é ${estiloMusical}`)
+}
+perfil("Lucas", 32, "eletrônica")
+
+function cultural(filme, musica){
+    console.log(`Adoro o filme ${filme} e a música ${musica}`)
+}
+cultural("'Vanilla Sky'", "'Sometimes'")
+
+function triplo(numero){
+    console.log(numero * 3)
+}
+triplo(78)  
